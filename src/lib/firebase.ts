bdac1dd48df57f0db22761ffd443ff53823910d6
BoredKevin <kevin@boredkevin.com>
2026-07-26 21:20:09 +0700

    remove hardcoded firebase fallbacks (oops!)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, signInAnonymously, onAuthStateChanged, User } from 'firebase/a
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBJ-Qu1mCA1Z4SkM40ANo3zOeupY5UqAg8",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "cravion-tv.firebaseapp.com",
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://cravion-tv-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "cravion-tv",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "cravion-tv.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "664308507663",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:664308507663:web:7ba5ce975ce874d35caf7a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
